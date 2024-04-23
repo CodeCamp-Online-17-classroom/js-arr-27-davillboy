9f5547d9774f626af9e3b23318b66e032afdacdc
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 22:36:27 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // write code here
+function updateTask(id, newName) {
+  const index = tasks.findIndex(item => item.id === id);
+  if (index !== -1) {
+    tasks[index].name = newName;
+  }
+}
+
+const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+
+updateTask(2, 'Travelling');
+console.log(tasks);
